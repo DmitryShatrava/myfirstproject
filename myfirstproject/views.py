4bf7d8c8a1d837521eeb3d98e39535c75079dd9c
DmitryShatrava <dshatrava@gmail.com>
2021-03-22 16:09:39 +0200

    Add reverse function

--- myfirstproject/views.py
+++ myfirstproject/views.py
@@ -8,7 +8,19 @@ def home(request):
     return render(request, 'home.html', {'greeting' : 'Hello!'})
 
 def reverse(request):
+    #user_text_list = []
     user_text = request.GET['usertext']
     reverse_text = user_text[::-1]
+    #user_text_list = user_text
+
+    split_list = user_text.split()
+
+    count = 0
+    for items in split_list:
+        count +=1
+    count_items = count
+
     return render(request, 'reverse.html', {'usertext' : user_text, 
-    'reverstext' : reverse_text},)
+    'reverstext' : reverse_text, 'countitems' : count_items},)
+
+
